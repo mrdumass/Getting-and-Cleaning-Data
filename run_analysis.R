c0558d5f0500fe3195a207a1391c994c2986fa56
kirk <kirk2bigcomputing.com>
2014-07-18 09:57:49 -0400

    start to build the code

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,5 @@
-## This code will turn the raw data set into a tidy data set ##
+## This code will read in the raw data                   ##
+## next it will turn that dat into a single data.frame   ##
+## then it will clean up that data frame to include only ##
+## those thing we are interested in and change the names ##
+## of the columns so they are easy to understand         ##
